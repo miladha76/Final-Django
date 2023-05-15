377c4f62256ffe113612dc81c31d1b14a0fc6a0d
milad hatami <miladhatami_ut@yahoo.com>
2023-05-15 05:03:29 -0700

    home.html added-static files added

--- final_site/final_site/settings.py
+++ final_site/final_site/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS= [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "static_cdn"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS= ['final_site\static']
 MEDIA_URL = "/media/"
 MEDIA_ROOT=BASE_DIR / "media"
 
