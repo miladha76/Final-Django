d8411d078d64b548ac863b474a89bd03375cb9ad
milad hatami <miladhatami_ut@yahoo.com>
2023-05-17 08:52:19 -0700

    base.html added- home page changed -include folder added

--- final_site/final_site/urls.py
+++ final_site/final_site/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
