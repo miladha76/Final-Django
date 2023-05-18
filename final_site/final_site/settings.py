b8f0c2a13c3bfb8e3ff74740f2fadfb44528db3a
milad hatami <miladhatami_ut@yahoo.com>
2023-05-18 07:04:08 -0700

    deleted order app- make store page dynamic

--- final_site/final_site/settings.py
+++ final_site/final_site/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'store',
     'discount',
     'cart',
-    'order',
+    
     
 ]
 
--- final_site/order/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import PaymentDetails, OrderDetails, OrderItem
-
-admin.site.register(PaymentDetails)
-admin.site.register(OrderDetails)
-admin.site.register(OrderItem)
