8b191f37d729ae24187e3b13fc910c38499d9b74
milad hatami <miladhatami_ut@yahoo.com>
2023-05-21 02:16:18 -0700

    add variation class to product model

--- final_site/order/models.py
+++ final_site/order/models.py
@@ -1,32 +1,63 @@
-from django.db import models
-from user.models import User
-from store.models import Product
+# from django.db import models
+# from user.models import User,UserAddress
+# from store.models import Product
 
-class OrderDetail(models.Model):
-    user = models.ForeignKey(User ,on_delete=models.CASCADE)
-    total= models.DecimalField( max_digits=10, decimal_places=2)
-    created_at =models.DateTimeField(auto_now_add=True)
-    modified_at=models.DateTimeField(auto_now=True)
+# class Payment(models.Model):
+#     user = models.ForeignKey(User,on_delete=models.CASCADE)
+#     payment_id = models.CharField(max_length=100)
+#     payment_method = models.CharField(max_length=100)
+#     amount_paid = models.CharField(max_length=100)
+#     status = models .CharField(max_length=100)  
+#     created_at=models.DateTimeField(auto_now_add=True)
     
-    def __str__(self) :
-        return f'{self.user}'
+#     def __str__(self):
+#         return self.payment_id
+
+# class Order(models.Model):
+#     STATUS = (
+#         ('New','New'),
+#         ('Accepted','Accepted'),
+#         ('Completed','Completed'),
+#         ('Cancelled','Cancelled'),
+#     )
+#     user = models.ForeignKey(User ,on_delete=models.SET_NULL,null=True)
+#     payment =models.ForeignKey(Payment ,on_delete=models.SET_NULL,blank=True,null=True)
+#     order_number = models.CharField(max_length=50)
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     phone = models.CharField(max_length=11)
+#     address_line1 =models.CharField(max_length=50)
+#     address_line2 =models.CharField(max_length=50,blank=True)
+#     country = models.CharField(max_length=50)
+#     city =models.CharField(max_length=50)
+#     order_note = models.CharField(max_length=100, blank=True)
+#     order_total = models.FloatField()
+#     status =models.CharField(max_length=10,choices=STATUS,default='New')
+#     tax = models.FloatField()
+#     ip=models.CharField(max_length=20,blank=True)
+#     is_ordered=models.BooleanField(default=False)
+#     created_at =models.DateTimeField(auto_now_add=True)
+#     updated_at=models.DateTimeField(auto_now=True)
     
-class OrderItem(models.Model):
-    order=models.ForeignKey(OrderDetail,on_delete=models.CASCADE)
-    product = models.ForeignKey(Product,on_delete=models.CASCADE)
-    created_at= models.DateTimeField(auto_now_add=True)   
-    modified_at=models.DateTimeField(auto_now=True)
+#     def __str__(self) :
+#         return self.user.first_name
     
-    def __str__(self):
-        return f'{self.product}'
+# class OrderItem(models.Model):
+#     order=models.ForeignKey(Order,on_delete=models.CASCADE)
+#     payment= models.ForeignKey(Payment ,on_delete=models.SET_NULL,blank=True,null=True)
+#     user = models.ForeignKey(User ,on_delete=models.CASCADE)
+#     product = models.ForeignKey(Product,on_delete=models.CASCADE)
+#     variation = models.ForeignKey(Variation ,on_delete=models.CASCADE)
+#     color = models.CharField(max_length=50)
+#     size =models.CharField(max_length=50)
+#     quantity = models . IntegerField()
+#     product_price = models.FloatField()
+#     ordered= models.BooleanField(default=False)
+#     created_at= models.DateTimeField(auto_now_add=True)   
+#     updated_at=models.DateTimeField(auto_now=True)
+    
+#     def __str__(self):
+#         return self.product.product_name
     
     
     
-class Payment(models.Model):
-    order=models.OneToOneField(OrderDetail,on_delete=models.CASCADE)
-    amount =models.IntegerField()
-    status =models.CharField(max_length=50,choices=[('d','done'),('f','failed'),('p','process')])   
-    created_at=models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return f'{self.order.user}s payment'
