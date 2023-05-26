173997e4c3bb44db56acd034a988fcdd4dba957f
milad hatami <miladhatami_ut@yahoo.com>
2023-05-26 14:56:57 -0700

    make activation link email for user

--- final_site/accounts/views.py
+++ final_site/accounts/views.py
@@ -3,6 +3,13 @@ from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages,auth
 from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
 
 def register(request):
     if request.method == 'POST':
@@ -17,8 +24,19 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
-            messages.success(request,'ثبت نام با موفقیت انجام شد')
-            return redirect('register')
+            
+            current_site = get_current_site(request)
+            mail_subject = 'لطفاً حساب کاربری خود را فعال کنید'
+            message = render_to_string('accounts/account_email_verification.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            return redirect('/accounts/login/?command=verification&email='+email)
     else:
         form = RegistrationForm()
     context = {
@@ -42,6 +60,22 @@ def login(request):
             return redirect('login')  
         
     return render(request,'accounts/login.html')
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'تبریک می‌گوییم! حساب کاربری شما فعال شده است.')
+        return redirect('login')
+    else:
+        messages.error(request, 'لینک فعال‌سازی نامعتبر است')
+        return redirect('register')
 
 
 @login_required(login_url = 'login')
