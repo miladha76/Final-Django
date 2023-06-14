14101e589913b489086779c899086aa47cc35c07
milad hatami <miladhatami_ut@yahoo.com>
2023-06-14 03:01:27 -0700

    make get profile with drf

--- final_site/accounts/api_views.py
+++ final_site/accounts/api_views.py
@@ -5,6 +5,9 @@ from django.shortcuts import get_object_or_404
 from .serializers import UserProfileSerializer
 from .models import UserProfile
 from rest_framework import viewsets
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from orders.models import Order
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
@@ -26,3 +29,17 @@ class UserProfileEditAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def dashboardd(request):
+    orders = Order.objects.order_by('-created_at').filter(user=request.user, is_ordered=True)
+    orders_count = orders.count()
+    user_profile = UserProfile.objects.get(user=request.user)
+
+    serializer = UserProfileSerializer(user_profile)
+    context = {
+        'orders_count': orders_count,
+        'userprofile': serializer.data,
+    }
+    return Response(context)
